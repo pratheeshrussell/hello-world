281b47e7a42d19bdacc1802880e1f775aca41e0a
Pratheesh <pratheeshrussell@gmail.com>
2016-02-26 12:44:40 +0530

    Added files via upload

---
+++ Head Or Tails/Head Or Tails/Form1.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(118, 235)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 34)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Toss"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(230, 214)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(83, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.PictureBox3)
+        Me.GroupBox1.Controls.Add(Me.PictureBox2)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(2, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(321, 204)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(130, 186)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Heads"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(130, 186)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Tails"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(77, 9)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(160, 165)
+        Me.PictureBox3.TabIndex = 4
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(77, 9)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(160, 165)
+        Me.PictureBox2.TabIndex = 3
+        Me.PictureBox2.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(225, 177)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Head"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(74, 177)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(24, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Tail"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-1, 9)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(322, 196)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(22, 246)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Reset"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(54, 217)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(43, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Heads/"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(103, 213)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(46, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(2, 214)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(46, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(324, 274)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Toss"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Head Or Tails/Head Or Tails/Form1.vb
@@ -0,0 +1,173 @@
+﻿Imports System.Text
+
+Public Class Form1
+    Dim p As String = Nothing
+    Dim q As Integer = Nothing
+    Dim RandomKey As String
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+    PictureBox1.Hide()
+        Label1.Hide()
+        Label2.Hide()
+        
+        TextBox2.Text = TextBox2.Text + 1
+        If q >= 3 Then
+            q = 0
+            If p = "h" Then
+                RandomKey = 2
+            ElseIf p = "t" Then
+                RandomKey = 1
+            End If
+        Else
+            Dim KeyGen As RandomKeyGenerator
+            Dim NumKeys As Integer
+            Dim i_Keys As Integer
+
+            NumKeys = 20
+
+            KeyGen = New RandomKeyGenerator
+            KeyGen.KeyLetters = "1"
+            KeyGen.KeyNumbers = "2"
+            KeyGen.KeyChars = 1
+            For i_Keys = 1 To NumKeys
+                RandomKey = KeyGen.Generate()
+            Next
+        End If
+        If RandomKey = 1 Then
+            heads()
+  ElseIf RandomKey = 2 Then
+            tails()
+        End If
+    End Sub
+    Function heads()
+
+        If p = "t" Or p = Nothing Then
+            p = "h"
+            q = "1"
+        ElseIf p = "h" Then
+            q = q + 1
+            'MsgBox(q)
+        End If
+
+        PictureBox2.Hide()
+        Label3.Hide()
+        TextBox1.Text = "Heads"
+
+        TextBox3.Text = TextBox3.Text + 1
+        PictureBox3.Show()
+        Label4.Show()
+    End Function
+    Function tails()
+        If p = "h" Or p = Nothing Then
+            p = "t"
+            q = "1"
+        ElseIf p = "t" Then
+            q = q + 1
+            'MsgBox(q)
+        End If
+
+
+        PictureBox3.Hide()
+        Label4.Hide()
+        TextBox1.Text = "Tails"
+        PictureBox2.Show()
+        Label3.Show()
+    End Function
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        PictureBox2.Hide()
+        PictureBox3.Hide()
+        TextBox1.Text = Nothing
+        TextBox2.Text = 0
+        TextBox3.Text = 0
+        Label3.Hide()
+        Label4.Hide()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        reset()
+
+    End Sub
+    Function reset()
+        PictureBox1.Show()
+        PictureBox2.Hide()
+        PictureBox3.Hide()
+        Label1.Show()
+        Label2.Show()
+        Label3.Hide()
+        Label4.Hide()
+        TextBox1.Text = ""
+        p = Nothing
+        q = Nothing
+        TextBox2.Text = 0
+        TextBox3.Text = 0
+        Me.Refresh()
+        Exit Function
+    End Function
+
+End Class
+Public Class RandomKeyGenerator
+    Dim Key_Letters As String
+    Dim Key_Numbers As String
+    Dim Key_Chars As Integer
+    Dim LettersArray As Char()
+    Dim NumbersArray As Char()
+
+    Protected Friend WriteOnly Property KeyLetters() As String
+        Set(ByVal Value As String)
+            Key_Letters = Value
+        End Set
+    End Property
+    Protected Friend WriteOnly Property KeyNumbers() As String
+        Set(ByVal Value As String)
+            Key_Numbers = Value
+        End Set
+    End Property
+    Protected Friend WriteOnly Property KeyChars() As Integer
+        Set(ByVal Value As Integer)
+            Key_Chars = Value
+        End Set
+    End Property
+    Function Generate() As String
+        Dim i_key As Integer
+        Dim Random1 As Single
+        Dim arrIndex As Int16
+        Dim sb As New StringBuilder
+        Dim RandomLetter As String
+        LettersArray = Key_Letters.ToCharArray
+        NumbersArray = Key_Numbers.ToCharArray
+
+        For i_key = 1 To Key_Chars
+
+
+            Randomize()
+            Random1 = Rnd()
+            arrIndex = -1
+
+            If (CType(Random1 * 111, Integer)) Mod 2 = 0 Then
+
+                Do While arrIndex < 0
+                    arrIndex = _
+                     Convert.ToInt16(LettersArray.GetUpperBound(0) _
+                     * Random1)
+                Loop
+                RandomLetter = LettersArray(arrIndex)
+                If (CType(arrIndex * Random1 * 99, Integer)) Mod 2 <> 0 Then
+                    RandomLetter = LettersArray(arrIndex).ToString
+                    RandomLetter = RandomLetter.ToUpper
+                End If
+                sb.Append(RandomLetter)
+            Else
+
+                Do While arrIndex < 0
+                    arrIndex = _
+                      Convert.ToInt16(NumbersArray.GetUpperBound(0) _
+                      * Random1)
+                Loop
+                sb.Append(NumbersArray(arrIndex))
+            End If
+        Next
+        Return sb.ToString
+    End Function
+
+End Class
